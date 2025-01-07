8f612ad1640ef064a237903ecb9e19ab945d6571
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-01-07 19:21:48 +0100

    [UPDATE] Constructor's

--- src/Sprites.js
+++ src/Sprites.js
@@ -1,6 +1,6 @@
 export default class Sprite
 {
-    constructor(id, state, xPos, yPos, imageSet, frames, physics)
+    constructor(id, state, xPos, yPos, imageSet, frames, physics, hitBox)
     {
         this.id =                 id;
         this.state =           state;
@@ -9,14 +9,20 @@ export default class Sprite
         this.imageSet =     imageSet;
         this.frames =         frames;
         this.physics =       physics;
+        this.hitBox =               hitBox;
+        this.isCollidingWithPlayer = false;
+        this.isCollidingWithObstacleOnTheTop = false;
+        this.isCollidingWithObstacleOnTheLeft = false;
+        this.isCollidingWithObstacleOnTheBottom = false;
+        this.isCollidingWithObstacleOnTheRight = false;      
     }
 }
 
 export class Enemies extends Sprite
 {
-    constructor(id, state, xPos, yPos, imageSet, frames, physics, maxTimeToChangeDirection)
+    constructor(id, state, xPos, yPos, imageSet, frames, physics, maxTimeToChangeDirection, hitBox)
     {
-        super(id, state, xPos, yPos, imageSet, frames, physics);
+        super(id, state, xPos, yPos, imageSet, frames, physics, hitBox);
         this.directionChangeCounter = 0;       
         this.maxTimeToChangeDirection = maxTimeToChangeDirection;
     }
