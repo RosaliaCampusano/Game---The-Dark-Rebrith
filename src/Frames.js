8605eb43f44ea08ec17b50a94c9c1470cd78626e
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-01-23 09:27:22 +0100

    [UPDATE] Frames class

--- src/Frames.js
+++ src/Frames.js
@@ -1,10 +1,10 @@
 export default class Frames{
     constructor(framesPerState, speed = 1)
     {
-        this.framesPerState =   framesPerState;   
-        this.frameCounter =                  0; 
-        this.speed =                     speed;
-        this.framesChangeCounter =           0;                 
-        
+    
+        this.framesPerState = framesPerState;   
+        this.frameCounter = 0;                  
+        this.speed = speed;
+        this.framesChangeCounter = 0;
     }
 }
