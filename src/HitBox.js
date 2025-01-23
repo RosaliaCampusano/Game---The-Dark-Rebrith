bb7277d8a351be22a03001d0082df805cad41dc8
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-01-23 09:51:56 +0100

    [UPDATE] HitBox class

--- src/HitBox.js
+++ src/HitBox.js
@@ -1,11 +1,12 @@
 export default class HitBox 
 {
-
-    constructor (xSize, ySize, xOffset, yOffset)
+    constructor (xSize, ySize, xOffset, yOffset, color = "red")
     {
         this.xSize = xSize;
         this.ySize = ySize;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
+        this.color = color;
+        this.defaultColor = color;
     }
 }
