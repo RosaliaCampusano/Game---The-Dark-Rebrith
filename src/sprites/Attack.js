98b20f413d91a3c8e0958736ab3b69e21092f571
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-02-25 18:18:16 +0100

    refactor: add function and delete the blue explotion to make the game more comfortable

--- src/sprites/Attack.js
+++ src/sprites/Attack.js
@@ -1,4 +1,4 @@
-import { SpriteID, State } from "../constants.js";
+import { Sound, SpriteID, State } from "../constants.js";
 import Frames from "../Frames.js";
 import globals from "../globals.js";
 import HitBox from "../HitBox.js";
@@ -10,11 +10,15 @@ export class Attack extends Sprite
 {
     countFrameAttack = 0;
     moveSpeed = 1.8;
-    blueExplotion = {};
     left = false;
     right = false;
     top = false;
     bottom = false;
+    attackDirection = null;  
+    attackSpeedX = 0;  
+    attackSpeedY = 0;  
+    isActive = false;
+
 
     constructor()
     {
@@ -32,158 +36,98 @@ export class Attack extends Sprite
     update()
     {
         super.update()
-        
-        for (let index = 0; index < globals.sprites.length; index++) 
-        {
-            const sprite = globals.sprites[index];
-            if (sprite.id == SpriteID.BLUE_EXPLOTION) 
-            {
-                this.blueExplotion = sprite
-                this.blueExplotion.update();
+       
+            if (this.isActive) {
+                this.updateXPos();
+                this.updateYPos();
+                this.countFrameAttack++;
+            }
+
+            if (this.isCollidingOrMaxCountFrameAttack()) {
+                this.defaultPositionAndFrame();
             }
         }
-
-        if (this.left)
-        {
-            this.leftAttack(globals.activedPlayer)
-        }
-
-        if (this.right)
-        {
-            this.rightAttack(globals.activedPlayer)
-        }
-
-        if (this.top)
-        {
-            this.upAttack(globals.activedPlayer)
-        }
-
-        if (this.bottom)
-        {
-            this.downAttack(globals.activedPlayer)
-        }
-    }
-
-    defaultPositionAndFrame(){
-        this.countFrameAttack = 0;
-        this.xPos = -10;
-        this.yPos = -10;
-        this.physics.vx = 0;
-        this.physics.vy = 0;
-    }
-
-    getPositionPlayer(player)
-    {
-        if (this.countFrameAttack == 0) {
+    
+        startAttack(player, direction) {
+           
+            if (this.isActive) return;
+    
+            this.isActive = true;
+            this.attackDirection = direction;
+            this.countFrameAttack = 0;
+    
+          
             this.xPos = player.xPos;
             this.yPos = player.yPos;
+    
+         
+            switch (direction) {
+                case 'left':
+                    this.attackSpeedX = -this.physics.vLimit * this.moveSpeed;
+                    this.attackSpeedY = 0;
+                    break;
+                case 'right':
+                    this.attackSpeedX = this.physics.vLimit * this.moveSpeed;
+                    this.attackSpeedY = 0;
+                    break;
+                case 'up':
+                    this.attackSpeedX = 0;
+                    this.attackSpeedY = -this.physics.vLimit * this.moveSpeed;
+                    break;
+                case 'down':
+                    this.attackSpeedX = 0;
+                    this.attackSpeedY = this.physics.vLimit * this.moveSpeed;
+                    break;
+            }
         }
-    }
+    
+        updateXPos() {
+            this.xPos += this.attackSpeedX * globals.deltaTime;
+        }
+    
+        updateYPos() {
+            this.yPos += this.attackSpeedY * globals.deltaTime;
+        }
 
-    isCollidingOrMaxCountFrameAttack()
-    {
-        return this.isCollidingWithObstacleOnTheLeft || 
-        this.isCollidingWithObstacleOnTheRight || 
-        this.isCollidingWithObstacleOnTheTop ||
-        this.isCollidingWithObstacleOnTheBottom ||
-        this.isCollidingWithSprite ||
-        this.countFrameAttack == 30;
-    }
+    
+    
+        defaultPositionAndFrame() {
+            this.countFrameAttack = 0;
+            this.xPos = -10;
+            this.yPos = -10;
+            this.attackSpeedX = 0;
+            this.attackSpeedY = 0;
+            this.isActive = false;
+        }
+    
 
-    updateXPos()
-    {
-        this.xPos += this.physics.vx * globals.deltaTime;
-    }
-
-    updateYPos()
-    {
-        this.yPos += this.physics.vy * globals.deltaTime;
-    }
-
-    leftAttack(player)
-    {
-        this.physics.vx = -this.physics.vLimit * this.moveSpeed;
-
-        this.getPositionPlayer(player)
-
-        this.countFrameAttack++;
-
-        this.updateXPos()
-        
-        
-        if (this.isCollidingOrMaxCountFrameAttack()) 
+        isCollidingOrMaxCountFrameAttack()
         {
-            player.state = State.STILL_LEFT_WIZARD
-            this.blueExplotion.getPosition(this.xPos, this.yPos)
-            this.defaultPositionAndFrame()
-            this.left = false;
+            return this.isCollidingWithObstacleOnTheLeft || 
+            this.isCollidingWithObstacleOnTheRight || 
+            this.isCollidingWithObstacleOnTheTop ||
+            this.isCollidingWithObstacleOnTheBottom ||
+            this.isCollidingWithSprite ||
+            this.countFrameAttack === 30;
         }
-    }
-
-    rightAttack(player)
-    {
-        this.physics.vx = this.physics.vLimit * this.moveSpeed;
-
-        this.getPositionPlayer(player)
-
-        this.countFrameAttack++;
-
-        this.updateXPos()
-
-        if (this.isCollidingOrMaxCountFrameAttack()) 
-        {
-            player.state = State.STILL_RIGHT_WIZARD
-            this.blueExplotion.getPosition(this.xPos, this.yPos)
-            this.defaultPositionAndFrame()
-        }
-    }
-
-    upAttack(player)
-    {
-        this.physics.vy = -this.physics.vLimit * this.moveSpeed;
-
-        this.getPositionPlayer(player);
-
-        this.countFrameAttack++;
-
-        this.updateYPos();
-
-        if (this.isCollidingOrMaxCountFrameAttack())
-        {
-            player.state = State.STILL_UP_WIZARD
-            this.blueExplotion.getPosition(this.xPos, this.yPos)
-            this.defaultPositionAndFrame()
-        }
-    }
-
-    downAttack(player)
-    {
-        this.physics.vy = this.physics.vLimit * this.moveSpeed;
-
-        this.getPositionPlayer(player);
-
-        this.countFrameAttack++;
-
-        this.updateYPos();
-
-        if (this.isCollidingOrMaxCountFrameAttack())
-        {
-            player.state = State.STILL_DOWN_WIZARD
-            this.blueExplotion.getPosition(this.xPos, this.yPos)
-            this.defaultPositionAndFrame()
-        }
-    }
 
     detectCollisionsBetweenSpriteAndSprite(sprite)
     {
         const isColliding = super.detectCollisionsBetweenSpriteAndSprite(sprite);
-        
+    
         sprite.isCollidingWithAttack = isColliding;
 
-        if (sprite.__proto__.__proto__.constructor.name === "Enemies" && sprite.isCollidingWithAttack)
+        if (sprite.id === SpriteID.GOBLIN || sprite.id === SpriteID.DEMON) 
         {
-            this.isCollidingWithSprite = sprite.isCollidingWithAttack || isColliding;
-            setTimeout(() => {this.isCollidingWithSprite = false}, 100);
+            if (sprite.isCollidingWithAttack) 
+            {
+              
+                if (sprite.id === SpriteID.GOBLIN) globals.currentSound = Sound.GOBLIN;
+                if (sprite.id === SpriteID.DEMON) globals.currentSound = Sound.DEMON;
+
+                this.isCollidingWithSprite = sprite.isCollidingWithAttack || isColliding;
+                this.isCollidingWithSprite = false;
+            }
         }
 
         return isColliding;
